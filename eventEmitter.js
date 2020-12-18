0513e047a04dc3c24e1a0eababc5d62e105249ac
Marilzon de Sousa <maxmaril@hotmail.com>
2020-12-18 19:28:20 -0300

    Update: eventEmitter

---
+++ eventEmitter.js
@@ -0,0 +1,18 @@
+import EventEmitter from 'events';
+
+class Users extends EventEmitter {
+  userLogged(data) {
+    setTimeout(() => {
+      this.emit('User logged', data);
+    }, 1000);
+  }
+}
+
+const users = new Users();
+
+users.on('User logged', data => {
+  console.log(data);
+});
+
+users.userLogged({ user: 'Marilzon de Sousa' });
+users.userLogged({ user: 'Cristiane dos Santos' });
