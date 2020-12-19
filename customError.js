d4a5b00de14cde41b0b6631629f45bc56c24ebb5
Marilzon de Sousa <maxmaril@hotmail.com>
2020-12-19 18:43:26 -0300

    Update: errors lessons

---
+++ customError.js
@@ -0,0 +1,23 @@
+class CustomError extends Error {
+  constructor({ message, data }) {
+    super(message);
+    this.data = data;
+  }
+}
+
+try {
+  const name = 'Marilzon de Sousa';
+
+  const myError = new CustomError({
+    message: 'Custom message on ustom error',
+    data: {
+      type: 'Server error'
+    }
+  });
+  throw myError;
+} catch (error) {
+  console.log(error);
+  console.log(error.data);
+} finally {
+  console.log('Keep going...');
+}
